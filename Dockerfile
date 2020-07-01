db2baf6fa309fbd9f91567c2aa6573417b3a5f45
dmattia <david@transcend.io>
2020-07-01 09:48:52 -0500

    Move env vars into entrypoint script

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM golang:1.14
 
-ENV FAST_MODE true
-ENV GO111MODULE on
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Install the tool
+export GO111MODULE=on
 go get "github.com/transcend-io/terragrunt-atlantis-config@$1"
 
 # Generate what the config should look like
 shift
+export FAST_MODE=true
 terragrunt-atlantis-config generate --output atlantis.yaml.expected "$@"
 
 # Enforce that the config looks how you'd expect
