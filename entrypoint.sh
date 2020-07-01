9502bdb01f0b64ee96cb3590a388acffa81f3a26
dmattia <david@transcend.io>
2020-07-01 10:02:21 -0500

    Expand CLI arguments properly

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ go get "github.com/transcend-io/terragrunt-atlantis-config@$1"
 # Generate what the config should look like
 shift
 export FAST_MODE=true
-terragrunt-atlantis-config generate --output atlantis.yaml.expected "$@"
+terragrunt-atlantis-config generate --output atlantis.yaml.expected $@
 
 # Enforce that the config looks how you'd expect
 diff atlantis.yaml atlantis.yaml.expected
